3c6fb5f48f875d3b83c1a1bf54b319636e35071b
choiyoorim <yulim3839@naver.com>
2023-05-26 14:25:19 +0900

    feat:✨ 친구 캘린더 피드 리스트 GET 기능 추가

--- models/calendar.js
+++ models/calendar.js
@@ -24,7 +24,63 @@ const getCommentAndReactionByMonth = async(month, userID) => {
     let [rows, fields] = await db.query(sql);
     return rows;
 }
+
+const getCalendarFeed = async(userID) => {
+    let sql = 
+    `
+    SELECT ch.cHistoryID, f.user1ID userID, f.user2ID writerID, u.nickname writerNickname, u.profile writerProfile,
+    ch.amount, ch.content, ch.category, ch.date 
+    FROM consumptionHistory ch, friend f, user u 
+    WHERE f.user1ID = ${userID} AND f.user2ID = ch.userID AND f.accepted = 0 AND secret = 0 AND u.userID = f.user2ID 
+    ORDER BY ch.date DESC 
+    `
+    let [rows, fields] = await db.query(sql);
+    return rows;
+}
+
+const getCountOfEmoticon = async(cHistoryID) => {
+    let sql = `SELECT count(*) count FROM emoticon e WHERE e.cHistoryID = ${cHistoryID} AND e.category = 0;`
+    let [rows] = await db.query(sql);
+    const positiveEmoticonCount = rows[0].count;
+    
+    sql = `SELECT count(*) count FROM emoticon e WHERE e.cHistoryID = ${cHistoryID} AND e.category = 1;`
+    [rows] = await db.query(sql);
+    const negativeEmoticonCount = rows[0].count;
+
+    return {
+        positiveEmoticonCount,
+        negativeEmoticonCount,
+    }
+}
+
+const getCountOfComment = async(cHistoryID) => {
+    let sql = `SELECT count(*) count FROM comment c WHERE c.cHistoryID = ${cHistoryID}`
+    let [rows] = await db.query(sql);
+    return rows[0].count;
+}
+
+const getCommentByHistoryID = async(cHistoryID) => {
+    let sql = `SELECT commentID, userID, content, date 
+    FROM comment c 
+    WHERE c.cHistoryID = ${cHistoryID}`
+    let [rows] = await db.query(sql);
+    return rows;
+}
+
+const getEmoticonByHistoryID = async(cHistoryID) => {
+    let sql = `SELECT emoticonID, userID, category
+    FROM emoticon e
+    WHERE e.cHistoryID = ${cHistoryID}`
+    let [rows] = await db.query(sql);
+    return rows;
+}
+
 module.exports = {
     getConsumptionHistoryByMonth,
-    getCommentAndReactionByMonth
+    getCommentAndReactionByMonth,
+    getCalendarFeed,
+    getCountOfComment,
+    getCountOfEmoticon,
+    getCommentByHistoryID,
+    getEmoticonByHistoryID
 }
