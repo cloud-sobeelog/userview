73b2fdd17be3676eb1e7c07c14cca12141b8368a
HwangHyeryeong <hrh4225@gmail.com>
2023-05-28 00:09:00 +0900

    feat:✨ 내친구목록 GET 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 
 router.use('/mycalendar', require('./mycalendar'));
 router.use('/consumptionhistory',require('./consumptionHistory')); 
+router.use('/friends', require('./friends'));
 module.exports = router;
