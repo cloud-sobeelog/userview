f881d8a9a34b890565ce788df48853d52dbdc420
HwangHyeryeong <hrh4225@gmail.com>
2023-05-30 22:23:39 +0900

    feat: ✨ 친구 닉네임 검색 GET 기능 추가

--- constants/responseMessage.js
+++ constants/responseMessage.js
@@ -33,6 +33,7 @@ module.exports = {
 
     //친구
     READ_FRIENDS_LIST_SUCCESS: '친구 목록 조회 성공',
+    READ_FIND_USERS_SUCCESS: '사용자 검색 조회 성공',
     
     // ConsumptionHistory 
     ADD_ONE_POST_SUCCESS: '포스트 추가 성공',
--- routes/friends/index.js
+++ routes/friends/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const myFriendsListGET = require('../../controllers/friends/myFriendsListGET');
+const findUserListGET = require('../../controllers/friends/findUserListGET');
 
+
+router.get('/search', findUserListGET);
 router.get('/:userid', myFriendsListGET);
 
 module.exports = router;
