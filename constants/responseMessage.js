fdd064024fadc3f1cc699ae07f8a8c6061579038
HwangHyeryeong <hrh4225@gmail.com>
2023-05-31 19:08:59 +0900

    feat: ✨친구 신청 POST 기능 추가

--- constants/responseMessage.js
+++ constants/responseMessage.js
@@ -35,6 +35,7 @@ module.exports = {
     READ_FRIENDS_LIST_SUCCESS: '친구 목록 조회 성공',
     READ_FIND_USERS_SUCCESS: '사용자 검색 조회 성공',
     READ_FRIEND_REQUESTS_SUCCESS: '친구 요청 목록 조회 성공',
+    REQUEST_FRIEND_SUCCESS: '친구 신청 성공',
     
     // ConsumptionHistory 
     ADD_ONE_POST_SUCCESS: '포스트 추가 성공',
--- routes/friends/index.js
+++ routes/friends/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const myFriendsListGET = require('../../controllers/friends/myFriendsListGET');
 const findUserListGET = require('../../controllers/friends/findUserListGET');
+const friendRequestPOST = require('../../controllers/friends/friendRequestPOST');
 const receivedFriendRequestsListGET = require('../../controllers/friends/receivedFriendRequestsListGET');
+const myFriendsListGET = require('../../controllers/friends/myFriendsListGET');
 
+router.post('/requests', friendRequestPOST);
 router.get('/search', findUserListGET);
 router.get('/requests/:userid', receivedFriendRequestsListGET);
 router.get('/:userid', myFriendsListGET);
