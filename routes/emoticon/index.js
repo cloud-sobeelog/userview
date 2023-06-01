072ec4179e51120cbcb871a90160c24e625ef28a
Han seyoung <20201016@sungshin.ac.kr>
2023-06-01 11:42:28 +0900

    bug: 🐛 module 오류 해결

--- routes/emoticon/index.js
+++ routes/emoticon/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const emoticonPOST = require('../../controllers/emoticon/emoticonPOST');
+const router = express.Router();
 
 router.post('/:cHistoryID', emoticonPOST); // 공감하기(좋아요/ 싫어요)
 module.exports = router;
