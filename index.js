677003b370016406cca602b1cad199720554d6fe
junhee lee <80439077+junheelee411@users.noreply.github.com>
2023-06-06 00:06:33 +0900

    feat: ✨ 비밀번호 해싱, DB 연결

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/mycalendar', require('./mycalendar'));
-router.use('/consumptionhistory',require('./consumptionHistory')); 
-router.use('/comment', require('./comment'));
-router.use('/calendarfeed',require('./calendarFeed'));
-router.use('/friends', require('./friends'));
-router.use('/emoticon', require('./emoticon'));
-router.use('/auth', require('./auth'));
-router.use('/user', require('./user'));
-
-
-module.exports = router;
+module.exports = {
+    consumptionHistoryDB: require('./consumptionHistory'),
+    emoticonDB: require('./emoticon'),
+    commentDB: require('./comment'),
+    calendarDB: require('./calendar'),
+    friendsDB: require('./friends'),
+}
---
+++ user.js
@@ -0,0 +1,19 @@
+const {db} = require("./db");
+
+const postjoin = async(userID, password, callback) => {
+
+  const salt = crypto.randomBytes(16).toString('hex'); //16바이트 난수 생성 후 문자열로 변환
+  const hashedPassword = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex'); //비밀번호 해싱 후 문자열로 변환
+
+  let sql = `INSERT INTO user
+  (userID, hashedPassword)
+  VALUES (${userID}, ${hashedPassword})`;
+
+  let [rows] = await db.query(sql);
+
+  console.log(rows);
+};
+
+module.exports = {
+    postjoin
+}
