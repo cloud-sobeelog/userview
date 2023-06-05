232fe9e16f4ca8e73164f59590e463c12c262ed7
choiyoorim <yulim3839@naver.com>
2023-06-05 18:19:51 +0900

    bug: 🐛 부모 댓글 replyID null 처리 로직 변경

--- controllers/comment/commentPOST.js
+++ controllers/comment/commentPOST.js
@@ -5,20 +5,13 @@ const { commentDB } = require("../../models");
 
 module.exports = async (req, res) => {
     try {
+        console.log(req);
         const {cHistoryID} = req.params;
-        const {userID, content, replyID} = req.body;
+        let {userID, content, replyID} = req.body;
 
         // replyID에 값이 없다면 댓글, 있다면 대댓글
-        if (replyID === undefined) {
+        if (replyID == 0) {
             replyID = null;
-        }
-
-        // 부모 댓글의 replyID가 NULL이 아님 -> 대댓글임
-        const check = await commentDB.getOneComment(replyID);
-
-        if (check[0].replyID != null) {
-            return res.status(statusCode.BAD_REQUEST)
-            .send(util.fail(statusCode.BAD_REQUEST, responseMessage.CANT_COMMENT));
         }
 
         // 글자 수 제한 확인
