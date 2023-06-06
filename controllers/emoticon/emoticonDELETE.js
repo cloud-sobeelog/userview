86c716e5475274e2302f018889f7495a61aa9c60
Han seyoung <20201016@sungshin.ac.kr>
2023-06-06 17:29:07 +0900

    bug: 🐛 이모티콘 수 가져오기, DELETE 수정

--- controllers/emoticon/emoticonDELETE.js
+++ controllers/emoticon/emoticonDELETE.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
         const {cHistoryID} = req.params;
         const {userID, category} = req.body;
 
-        const check = await emoticonDB.isUserEmoticon(cHistoryID, userID);
+        const check = await emoticonDB.isUserEmoticon(cHistoryID, userID, category);
         
         if (check.length > 0) { // 이미 공감한 적이 있는 소비내역인지 확인
             if (check[0].category == category) { // 전에 공감한 것과 같은 것을 선택했는지 확인
@@ -16,6 +16,14 @@ module.exports = async (req, res) => {
 
                 return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.DELETE_CMOTICON_SUCCESS, result));
             }
+            else {
+                return res.status(statusCode.BAD_REQUEST)
+                .send(util.fail(statusCode.BAD_REQUEST, responseMessage.DELETE_CMOTICON_FAIL));
+            }
+        }
+        else {
+            return res.status(statusCode.BAD_REQUEST)
+            .send(util.fail(statusCode.BAD_REQUEST, responseMessage.DELETE_CMOTICON_FAIL));
         }
     }
     catch(err) {
