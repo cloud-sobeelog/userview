7a1f03bc9eb6635d73c8c191eb513a446a308524
choiyoorim <yulim3839@naver.com>
2023-06-06 21:28:22 +0900

    bug: 🐛 이모티콘 카테고리 미수정

--- controllers/calendarFeed/calendarFeedGET.js
+++ controllers/calendarFeed/calendarFeedGET.js
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
                 result[index].date = dateFormat(result[index].date);
                 result[index].commentCount = (await calendarDB.getCountOfComment(result[index].cHistoryID))[0].count;
                 result[index].positiveEmoticonCount = (await calendarDB.getCountOfEmoticon(result[index].cHistoryID,0));
-                result[index].negativeEmoticonCount = (await calendarDB.getCountOfEmoticon(result[index].cHistoryID,0));
+                result[index].negativeEmoticonCount = (await calendarDB.getCountOfEmoticon(result[index].cHistoryID,1));
                 result[index].comment = (await calendarDB.getCommentByHistoryID(result[index].cHistoryID));    
                 result[index].comment.forEach(element => element.date = dateFormat(element.date))            
                 result[index].emoticon = (await calendarDB.getEmoticonByHistoryID(result[index].cHistoryID));
